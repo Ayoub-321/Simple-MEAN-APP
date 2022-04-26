26231432c927bdead74c1f71164d9a993bae5f9b
Ayoub-321 <ayoubaissou.info@gmail.com>
2022-04-26 10:37:09 +0100

    Initial Commit

---
+++ src/app/posts/post.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Post } from './post.model';
+import { Router } from '@angular/router';
+
+import { environment } from 'src/environments/environment';
+
+const BACKEND_URL = environment.apiUrl + "/posts/";
+
+@Injectable({providedIn:'root'})
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  constructor(private http: HttpClient, private router: Router){}
+
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: any }>(BACKEND_URL)
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id,
+            imagePath: post.imagePath,
+            creator: post.creator
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  getPostUpdateListener()
+  {
+    return this.postsUpdated.asObservable();
+  }
+
+  getPost(id: string) {
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      imagePath: string;
+      creator: string;
+    }>(BACKEND_URL + id);
+  }
+
+  addPost(title: string, content: string, image: File) {
+    // const post: Post = { id: null, title: title, content: content };
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    postData.append("image", image, title);
+    this.http
+      .post<{ message: string; post: Post }>(BACKEND_URL, postData)
+      .subscribe(responseData => {
+        // const post: Post = {
+        //   id: responseData.post.id,
+        //   title: title,
+        //   content: content,
+        //   imagePath: responseData.post.imagePath
+        // };
+        // // const id = responseData.postId;
+        // // post.id = id;
+        // this.posts.push(post);
+        // this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string, image: File | string) {
+    // const post: Post = { id: id, title: title, content: content, imagePath: null };
+    let postData: Post | FormData;
+    if(typeof(image) === 'object') {
+      postData = new FormData();
+      postData.append("id", id);
+      postData.append("title", title);
+      postData.append("content", content);
+      postData.append("image", image, title);
+    } else {
+        postData = {
+        id: id,
+        title: title,
+        content: content,
+        imagePath: image,
+        creator: null
+      };
+    }
+    this.http
+      .put(BACKEND_URL + id, postData)
+      .subscribe(response => {
+        // const updatedPosts = [...this.posts];
+        // const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
+        // const post: Post = {
+        //   id: id,
+        //   title: title,
+        //   content: content,
+        //   imagePath: ""
+        // };
+        // updatedPosts[oldPostIndex] = post;
+        // this.posts = updatedPosts;
+        // this.postsUpdated.next([...this.posts]);
+        this.router.navigate(["/"]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete(BACKEND_URL + postId)
+      .subscribe(() =>{
+        const updatedPost = this.posts.filter(post => post.id !== postId);
+        this.posts = updatedPost;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+}
